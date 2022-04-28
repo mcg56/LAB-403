3c94d1859c3dcba174fed4ada7e6d99aba9891b7
Angus Eason <aje87@uclive.ac.nz>
2022-04-29 11:30:57 +1200

    Adding code for tracking reference gain N and now have correct step plots to 0.1m

--- inverted_pendulum.m
+++ inverted_pendulum.m
@@ -12,6 +12,7 @@ k_g = 3.71;     %(m/m)      Gearing ratio
 k_m = 0.0168;   %(V.s/rad)  Motor back EMF constant
 c   = 0;        %(N.s/m)    Damping on cart
 g   = 9.81;     %(m/s^2)    Acceleration due to gravity
+dx  = 0.1;      %(m)        Step size of cart.
 
 %Matricies
 A3_2 = (-m_p^2*L^2*g)/((m_c + m_p)*I_o + m_c*m_p*L^2);
@@ -29,20 +30,31 @@ B4 = m_p*L*k_m*k_g/(((m_c + m_p)*I_o + m_c*m_p*L^2)*R*r);
 
 B = [0;0;B3;B4];
 
-C = eye(4);
+C = [1 0 0 0]; %eye(4); %make it eye(4) for the real thing, make it [1 0 0 0] so that only x is plotted for easy reading in matlab
 D = 0;
 
 %% Make state space object
 sys = ss(A, B, C, D);
 
 %check stability
-eig(sys)
+eig(sys);
 
 %% Design control gains
+%Place poles method
+%poles = [-1+5i, -1-5i, -1, -2];
+poles = [-3 -4 -5 -6];
+K = place(A, B, poles);
+ACL = A - B*K;
+sys = ss(ACL, B, C, D);
 
-
-
+%tracking gain N
+C1 = [1 0 0 0];
+N = -inv(C1*inv(ACL)*B); 
 
 %% Responses
-% figure
-% impulse(sys);
+
+sys.B = dx*N*sys.B; %As it is tracking a dx step input, also include tracking gain N
+
+
+figure
+step(sys);
